d8c7037d7b1aa73a2b50fbbeca4129e8a9351f29
Michael Beine <michael.beine@bas.bund.de>
2024-12-09 18:50:24 +0100

    Setzen von Proxy und Ausnahmen

--- src/test/java/de/bas/schulung/playwright/BasPlaywrightOptions.java
+++ src/test/java/de/bas/schulung/playwright/BasPlaywrightOptions.java
@@ -4,12 +4,17 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.junit.Options;
 import com.microsoft.playwright.junit.OptionsFactory;
+import com.microsoft.playwright.options.Proxy;
 
 public class BasPlaywrightOptions implements OptionsFactory {
     @Override
     public Options getOptions() {
         var newContextOptions = new Browser.NewContextOptions();
-        newContextOptions.setProxy("http://10.12.184.56:8001/");
+
+        Proxy proxy = new Proxy("http://10.12.184.56:8001/");
+        proxy.setBypass("localhost,.bvaetw.de,.bvaint.de,.bva.de,.internal");
+        newContextOptions.setProxy(proxy);
+
         var connectOptions = new BrowserType.ConnectOptions();
         connectOptions.setTimeout(10000);
 
