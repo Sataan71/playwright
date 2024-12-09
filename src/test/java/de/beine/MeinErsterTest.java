bc1611d4e10942023337d86fb5a3f88b1c4191df
sataan71 <m.beine@gmx.net>
2024-12-09 14:09:58 +0100

    ... codegen + 'zweitesSzenario'

--- src/test/java/de/beine/MeinErsterTest.java
+++ src/test/java/de/beine/MeinErsterTest.java
@@ -4,10 +4,13 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
+@UsePlaywright
 public class MeinErsterTest {
 
     @Test
@@ -24,5 +27,14 @@ public class MeinErsterTest {
 
             assertThat(page).hasURL("https://www.heise.de/");
         }
+    }
+
+    @Test
+    void zweitesSzenario(Page page) {
+        page.navigate("https://www.wikipedia.de/");
+        page.getByLabel("Wikipedia durchsuchen").click();
+        page.getByLabel("Wikipedia durchsuchen").fill("Hallo Welt!");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Suchen")).click();
+        assertThat(page.locator("#firstHeading")).containsText("Hallo Welt!");
     }
 }
